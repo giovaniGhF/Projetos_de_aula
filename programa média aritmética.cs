1e074d79923452743da2ed42a5f020bceb665cec
giovaniGhF <79146937+giovaniGhF@users.noreply.github.com>
2025-09-20 10:29:13 -0300

    Add files via upload
    Na última sexta-feira, dia 19 de setembro de 2025, na faculdade fizemos como atividade um programa que calcula a média de 4 notas de um aluno como variável double. Calculamos e exibimos sua média final, sua menção menção(podendo ser A, B, C, D, ou E) e também se o aluno foi aprovado ou reprovado.
    Para esta atividade usamos a linguagem C# no modo console.

---
+++ programa média aritmética.cs
@@ -0,0 +1,71 @@
+﻿Console.Write("Digite o nome do aluno: ");
+string nome = Console.ReadLine();
+Console.Write("Digite a nota 1 do aluno: ");
+double nota1 = double.Parse(Console.ReadLine());
+Console.Write("Digite a nota 2 do aluno: ");
+double nota2 = double.Parse(Console.ReadLine());
+Console.Write("Digite a nota 3 do aluno: ");
+double nota3 = double.Parse(Console.ReadLine());
+Console.Write("Digite a nota 4 do aluno: ");
+double nota4 = double.Parse(Console.ReadLine());
+
+double media_final = (nota1 + nota2 + nota3 + nota4) / 4;
+
+if (media_final >= 9 && media_final <= 10)
+{
+    Console.WriteLine("==========================================");
+    Console.WriteLine("             BOLETIM ESCOLAR              ");
+    Console.WriteLine("==========================================");
+    Console.WriteLine($"Aluno: {nome}");
+    Console.WriteLine($"NotaS: {nota1} | {nota1} | {nota2} | {nota1}");
+    Console.WriteLine($"Média: {media_final}");
+    Console.WriteLine($"Menção: A");
+    Console.WriteLine("Situação: Aprovado");
+}
+
+else if (media_final >= 7 && media_final < 9)
+{
+    Console.WriteLine("==========================================");
+    Console.WriteLine("             BOLETIM ESCOLAR              ");
+    Console.WriteLine("==========================================");
+    Console.WriteLine($"Aluno: {nome}");
+    Console.WriteLine($"NotaS: {nota1} | {nota1} | {nota2} | {nota1}");
+    Console.WriteLine($"Média: {media_final}");
+    Console.WriteLine($"Menção: B");
+    Console.WriteLine("Situação: Aprovado");
+}
+else if (media_final >= 5 && media_final < 7)
+{
+    Console.WriteLine("==========================================");
+    Console.WriteLine("             BOLETIM ESCOLAR              ");
+    Console.WriteLine("==========================================");
+    Console.WriteLine($"Aluno: {nome}");
+    Console.WriteLine($"NotaS: {nota1} | {nota1} | {nota2} | {nota1}");
+    Console.WriteLine($"Média: {media_final}");
+    Console.WriteLine($"Menção: C");
+    Console.WriteLine("Situação: Recuperação");
+}
+else if (media_final >= 3 && media_final < 5)
+{
+    Console.WriteLine("==========================================");
+    Console.WriteLine("             BOLETIM ESCOLAR              ");
+    Console.WriteLine("==========================================");
+    Console.WriteLine($"Aluno: {nome}");
+    Console.WriteLine($"NotaS: {nota1} | {nota1} | {nota2} | {nota1}");
+    Console.WriteLine($"Média: {media_final}");
+    Console.WriteLine($"Menção: D");
+    Console.WriteLine("Situação: Reprovado");
+}
+else if (media_final > 3)
+{
+    Console.WriteLine("==========================================");
+    Console.WriteLine("             BOLETIM ESCOLAR              ");
+    Console.WriteLine("==========================================");
+    Console.WriteLine($"Aluno: {nome}");
+    Console.WriteLine($"Notas: {nota1} | {nota1} | {nota2} | {nota1}");
+    Console.Write($"Média: {media_final}");
+    Console.Write($"Menção: D");
+    Console.WriteLine("Situação: Reprovado");
+
+}
+ 
